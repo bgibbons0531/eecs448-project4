0f19a1d5ee513b446b25a8fc36cf65ef0e280444
ChristianPorras <cjincolorado@comcast.net>
2016-11-03 15:30:37 -0500

    Enemy class created. Orc adjusted to account for new class

---
+++ src/bgibbons/game/entities/Enemy.java
@@ -0,0 +1,169 @@
+package bgibbons.game.entities;
+
+import bgibbons.game.graphics.Colors;
+import bgibbons.game.graphics.Font;
+import bgibbons.game.graphics.Screen;
+import bgibbons.game.level.Level;
+
+/**
+ * An extension of the Mob class for an orc.
+ * @author Chris Porras based on Player.java
+ * @version 1.0 20 October 2016.
+ */
+
+public abstract class Enemy extends Mob {
+
+	protected int xTile;
+	protected int yTile;
+	protected int scale = 1;
+	protected int color;
+
+
+	/**
+	 * Constructor the Enemy object.
+	 * @param level 	Level for the Enemy to be added to.
+	 * @param x 		The x coordinate the Enemy will start at.
+	 * @param y 		The y coordinate the Enemy will start at.
+	 * @param xTile 	The x coordinate for the sprite to start at.
+	 * @param yTile 	The y coordinate for the sprite to start at.
+	 * @param color 	The color to apply to the enemy sprite.
+	 * @param maxHealth The maximum health for the enemy.
+	 * @param speed 	The speed of the enemy.
+	 * @param rank 		The starting rank of the enemy.
+	 * @param maxExp	The maximum experience of the enemy.
+	 */
+	public Enemy(Level level, int x, int y, int xTile, int yTile, int color, int maxHealth, int speed, int rank, int maxExp) {
+		super(level, "Enemy", x, y, maxHealth, speed, rank, maxExp);
+		this.xTile = xTile;
+		this.yTile = yTile;
+		this.color = color;
+	}
+
+	/**
+	 * Ticks the Enemy
+	 */
+	public void tick() {
+		// Level orc
+		if (currentExp >= maxExp) {
+			rankUp();
+		}
+
+		// Move orc
+		int xa = 0;
+		int ya = 0;
+
+		if (xa != 0 || ya != 0) {
+			move(xa, ya);
+			isMoving = true;
+		} else {
+			isMoving = false;
+		}
+
+		if (level.getTile(this.x >> 3, this.y >> 3).getId() == 3) {
+			isSwimming = true;
+		}
+		if  (isSwimming && level.getTile(this.x >> 3, this.y >> 3).getId() != 3) {
+			isSwimming = false;
+		}
+		tickCount++;
+	}
+
+	/**
+	 * Renders the Enemy to the screen
+	 * @param screen 	The screen to render the Enemy to.
+	 */
+	public void render(Screen screen) {
+		int walkingSpeed = 4;
+		int flipTop = (numSteps >> walkingSpeed) & 1;
+		int flipBottom = (numSteps >> walkingSpeed) & 1;
+
+		if (movingDir == 0) {
+			xTile += 2;
+		} else if (movingDir == 1) {
+			xTile += 6;
+		} else if (movingDir > 1) {
+			xTile += 8 + ((numSteps >> walkingSpeed) & 1) * 2;
+			flipTop = (movingDir - 1) % 2;
+		}
+
+		if (!isMoving) {
+			if (lastDir == 0) {
+				xTile = 0;
+			} else if (lastDir == 1) {
+				xTile = 4;
+			} else {
+				xTile = 8;
+			}
+		}
+
+		int modifier = 8 * scale;
+		int xOffset = x - modifier/2 - 4;
+		int yOffset = y - modifier/2 - 4;
+
+		if (isSwimming) {
+			int waterColor = 0;
+			yOffset += 4;
+			if (tickCount % 60 < 15) {
+				yOffset -= 1;
+				waterColor = Colors.get(-1,-1,225,-1);
+			} else if (15 <= tickCount%60 && tickCount%60 < 30) {
+				waterColor = Colors.get(-1, 225, 115, -1);
+			} else if (30 <= tickCount%60 && tickCount%60 < 45) {
+				waterColor = Colors.get(-1, 115, -1, 225);
+			} else {
+				waterColor = Colors.get(-1, 225, 115, -1);
+			}
+			screen.render(xOffset, yOffset + 3, 31+29*32, waterColor, 0x00, 1);
+			screen.render(xOffset+8, yOffset + 3, 31+29*32, waterColor, 0x01, 1);
+		}
+
+		screen.render(xOffset + (modifier * flipTop), yOffset, xTile + yTile * 32, color, flipTop, scale); //Top left
+		screen.render(xOffset + modifier - (modifier * flipTop), yOffset, xTile + 1 + yTile * 32, color, flipTop, scale); //Top right
+
+		if (!isSwimming) {
+			screen.render(xOffset + (modifier * flipBottom), yOffset + modifier, xTile + (yTile + 1) * 32, color, flipBottom, scale); //Bottom left
+			screen.render(xOffset + modifier - (modifier * flipBottom), yOffset + modifier, xTile + 1 + (yTile + 1) * 32, color, flipBottom, scale); //Bottom right
+		}
+	}
+
+	/**
+	 * Checks if the Enemy will collide when moving.
+	 * @param xa 	The x direction the Enemy wants to move.
+	 * @param ya 	The y direction the Enemy wants to move.
+	 */
+	public boolean hasCollided(int xa, int ya) {
+		int xMin = -4;
+		int xMax = 3;
+		int yMin = 3;
+		int yMax = 7;
+
+		for (int x=xMin; x<xMax; x++) {
+			if (isSolidTile(xa, ya, x, yMin)) {
+				return true;
+			}
+		}
+
+		for (int x=xMin; x<xMax; x++) {
+			if (isSolidTile(xa, ya, x, yMax)) {
+				return true;
+			}
+		}
+
+		for (int y=yMin; y<yMax; y++) {
+			if (isSolidTile(xa, ya, xMin, y)) {
+				return true;
+			}
+		}
+
+		for (int y=yMin; y<yMax; y++) {
+			if (isSolidTile(xa, ya, xMax, y)) {
+				return true;
+			}
+		}
+
+
+		return false;
+	}
+
+	public abstract void rankUp();
+}
