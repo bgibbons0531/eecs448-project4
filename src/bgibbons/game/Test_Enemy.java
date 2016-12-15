7fa9351c9ffd73218ae3cdf7f53353a353414ddf
Christian-Porras <cjincolorado@comcast.net>
2016-12-14 20:46:12 -0600

    Test for orc movement added

--- src/bgibbons/game/Test_Enemy.java
+++ src/bgibbons/game/Test_Enemy.java
@@ -16,10 +16,65 @@ public class Test_Enemy {
    */
   public void run(){
     System.out.println("---------Starting Enemy Testing Suite---------");
+    System.out.println("testOrcStartingPos - " + testOrcStartingPos());
+    System.out.println("testBanditStartingPos - " + testBanditStartingPos());
+    System.out.println("testVampireStartingPos - " + testVampireStartingPos());
     System.out.println("testRespawnOrcs - " + testRespawnOrcs());
     System.out.println("testSpawnBandits - " + testSpawnBandits());
     System.out.println("testSpawnVampires - " + testSpawnVampires());
+    System.out.println("testEnemyMovement - " + testEnemyMovement());
     }
+
+  /**
+   * Tests Orcs starting position
+   * @return boolean, true if orc starts in correct position, false otherwise
+   */
+  public boolean testOrcStartingPos(){
+    Level testLevel = new Level("/res/levels/test_level.png", null, false);
+    Entity testOrc = new Orc(testLevel, 24*8, 48*8);
+    testLevel.addEntity(testOrc);
+    if(testLevel.entities.get(0).x == 24*8 && testLevel.entities.get(0).y == 48*8){
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Tests bandits starting position
+   * @return boolean, true if bandits starts in correct position, false otherwise
+   */
+  public boolean testBanditStartingPos(){
+    Level testLevel = new Level("/res/levels/test_level.png", null, false);
+    Entity testBandit = new Bandit(testLevel, 24*8, 48*8);
+    testLevel.addEntity(testBandit);
+    if(testLevel.entities.get(0).x == 24*8 && testLevel.entities.get(0).y == 48*8){
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Tests vampires starting position
+   * @return boolean, true if vampires starts in correct position, false otherwise
+   */
+  public boolean testVampireStartingPos(){
+    Level testLevel = new Level("/res/levels/test_level.png", null, false);
+    Entity testVampire = new Vampire(testLevel, 24*8, 48*8);
+    testLevel.addEntity(testVampire);
+    if(testLevel.entities.get(0).x == 24*8 && testLevel.entities.get(0).y == 48*8){
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
 
   /**
    * Tests that respawn enemies respawns correct amount of enemies
@@ -76,5 +131,40 @@ public class Test_Enemy {
     else{
       return false;
     }
+  }
+
+  /**
+   * Tests that enemy movement works correctly
+   * @return boolean, true if movement works correctly on orcs
+   */
+  public boolean testEnemyMovement(){
+    Level testLevel = new Level("/res/levels/test_level.png", null, false);
+    Entity e = new Orc(testLevel, 32*8, 32*8);
+    testLevel.addEntity(e);
+    while(((Orc)e).getDirection() == 4){
+      ((Orc)e).move();
+    }
+    int direction = ((Orc)e).getDirection();
+    if(direction == 0){
+      if(e.y == 32*8+1){
+        return true;
+      }
+    }
+    else if(direction == 1){
+      if(e.y == 32*8-1){
+        return true;
+      }
+    }
+    else if(direction == 2){
+      if(e.x == 32*8+1){
+        return true;
+      }
+    }
+    else if(direction == 3){
+      if(e.x == 32*8-1){
+        return true;
+      }
+    }
+    return false;
   }
 }
